975ba19b686a743e40fa0e2c4662365cd102cbca
Abdullahi Adam <AbdullahiAdam.aa@gmail.com>
2021-09-24 00:34:14 +0100

    Crypto-World, The one-stop app for all information on cryptocurrency

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Button, Menu, Typography, Avatar } from "antd";
+import { Link } from "react-router-dom";
+import {
+  HomeOutlined,
+  MoneyCollectOutlined,
+  BulbOutlined,
+  FunctionOutlined,
+  MenuOutlined,
+} from "@ant-design/icons";
+
+const Navbar = () => {
+  return <div></div>;
+};
+
+export default Navbar;
